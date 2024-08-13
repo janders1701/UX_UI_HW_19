8037766a223c5533bbaf7fd20769504c25a3e491
JB <163941305+janders1701@users.noreply.github.com>
2024-08-13 16:29:29 -0500

    added jquery

--- portfolio19/js/index.js
+++ portfolio19/js/index.js
@@ -1,3 +1,36 @@
-$(document).ready(function() {
-    $("#avatar").fadeIn(2000);
+// PRELOADER
+    function fade() {
+        $(".preloader").fadeOut("slow");
+        }
+        setTimeout(fade, 2000);
+
+    
+//DOWNLOAD BUTTON LOADING BAR
+const btnUploadFile = document.querySelector(".buttonDL");
+
+function setButtonProgress(button, percent) {
+    const textElement = button.querySelector(".button-text");
+    const progressBar = button.querySelector(".button-progress");
+
+    progressBar.style.width = `${percent}%`;
+
+    if (percent >= 0) {
+        textElement.textContent = button.dataset.progressText;
+    }
+
+    if (percent >= 100) {
+        textElement.textContent = button.dataset.completeText;
+    }
+}
+
+btnUploadFile.addEventListener('click', function() {
+    let progress = 0;
+    const interval = setInterval(function() {
+        if (progress >= 100) {
+            clearInterval(interval);
+        } else {
+            progress += 1;  
+            setButtonProgress(btnUploadFile, progress);
+        }
+    }, 10);
 });
